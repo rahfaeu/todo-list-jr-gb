32ccb4068451e09b3bd46ab0a669fc0ccf7095c1
rahfaeu <rahfaeu@gmail.com>
2025-07-11 16:39:01 -0300

    :tada: first commit

---
+++ src/main/java/br/com/rahfaeu/todo_list_jr_gb/TodoListJrGbApplication.java
@@ -0,0 +1,13 @@
+package br.com.rahfaeu.todo_list_jr_gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListJrGbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListJrGbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rahfaeu/todo_list_jr_gb/TodoListJrGbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rahfaeu.todo_list_jr_gb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJrGbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
